0c4f8e6212eb28211f1876ef760dbc0b7f627162
Daniel Vogelnest <daniel.vogelnest@me.com>
2024-08-13 12:14:00 +1000

    Improve logging

--- compress-videos.sh
+++ compress-videos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
 if [ $# -lt 1 ]; then
     echo "ERROR: At least 1 video path must be specified as an argument"
     exit 1
 fi
 
+echo "Compressing $# videos"
+video_index=1
 for video_path in "$@"; do
     filesize=$(stat -f%z "$video_path")
-    echo -e "\nCompressing $(basename "$video_path") ($(echo "scale=2; $filesize / 1024 / 1024" | bc)MB)"
+    echo -e "\nCompressing $(basename "$video_path") ($(echo "scale=2; $filesize / 1024 / 1024" | bc)MB) - video $video_index of $#"
     ffmpeg -hide_banner -stats -loglevel error \
         -i "$video_path" \
         -vcodec libx265 -x265-params log-level=error \
@@ -16,6 +17,6 @@ for video_path in "$@"; do
         "$video_path.compressed.mp4"
     compressed_filesize=$(stat -f%z "$video_path.compressed.mp4")
     echo "$(basename "$video_path") compressed to $(echo "scale=2; $compressed_filesize / 1024 / 1024" | bc)MB ($(echo "scale=2; $compressed_filesize / $filesize" | bc)x the size of the original)"
+    video_index=$((video_index + 1))
 done
-
 echo "Compression complete"
--- merge-videos.sh
+++ merge-videos.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
 if [ $# -lt 2 ]; then
   echo "ERROR: at least 2 video paths must be specified as arguments"
   exit 1
 fi
 
+echo "Merging $# videos"
 for video in "$@"; do
   echo file \'$video\' >>merge-videos.txt
 done
-
 # TODO: make this work for videos of different types and speeds
 ffmpeg -hide_banner -stats -loglevel error \
   -f concat -safe 0 -i merge-videos.txt \
   -c copy "$1".merged.mp4 \
   -movflags use_metadata_tags -map_metadata 0
-
 echo "Merge completed"
 rm merge-videos.txt
