f06032a4c049bd24b5d7997d8be2592ae97acf41
Daniel Vogelnest <daniel.vogelnest@me.com>
2024-10-03 12:09:06 +1000

    Handle speed change for video that doesn't have an audio track

--- adjust-video-speed.sh
+++ adjust-video-speed.sh
@@ -10,10 +10,17 @@ if ! [[ $2 =~ ^[0-9]+(\.[0-9]+)?$ ]] || (($(echo "$2 < 0.5" | bc -l) || $(echo "
 fi
 
 echo "Adjusting speed of $(basename "$1") to $2x"
+filter="[0:v]setpts=(1/$2)*PTS[v]"
+maps="-map [v]"
+audio=$(ffprobe -v error -select_streams a:0 -show_entries stream=index -of csv=p=0 "$1")
+if [ -n "$audio" ]; then
+    filter="$filter;[0:a]atempo=$2[a]"
+    maps="$maps -map [a]"
+fi
 ffmpeg -hide_banner -stats -loglevel error \
     -i "$1" \
     -vcodec libx265 -x265-params log-level=error -pix_fmt yuv420p -vtag hvc1 -crf 28 \
     -movflags use_metadata_tags -map_metadata 0 \
-    -filter_complex "[0:v]setpts=(1/$2)*PTS[v];[0:a]atempo=$2[a]" -map "[v]" -map "[a]" \
+    -filter_complex "$filter" $maps \
     "${1%.*}_$2x-speed.mp4"
 echo "Speed adjustment completed"
